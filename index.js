0c903312b1c3709ef8a9b4bf6153b2b9f845cbb6
cyb2rZ <alisheroromov185@gmail.com>
2024-05-01 23:51:49 +0500

    First section was completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('DONE')
+const getArgs = require('./helpers/args')
+const weatherCLI = () => {
+  const args = getArgs(process.argv)
+  console.log(args)
+
+  if(args.h) {
+    // help
+  }
+
+  if(args.t) {
+    // save token
+  }
+
+  if(args.s) {
+    // save city
+  }
+
+}
+
+weatherCLI()
